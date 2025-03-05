742625e63786716f5f380fe0ff23dc74f3d77f7d
Noah O'Neill <noahnogg@gmail.com>
2025-03-04 23:55:34 -0500

    I started creating the asteroid manager class

---
+++ DiurnalOrbit/AsteroidManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiurnalOrbit
+{
+    internal class AsteroidManager
+    {
+        private List<Asteroid> asteroids;
+        private Vector2 spawnBounds;
+
+        private float speed;
+        private Vector2 velocity;
+
+        private float minScale;
+        private float maxScale;
+
+        public AsteroidManager(float speed, float minScale, float maxScale)
+        {
+            this.speed = speed;
+
+
+            this.maxScale = maxScale;
+        }
+    }
+}
